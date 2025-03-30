095844c93f503a2db3213c8b39200501ed3684cd
Vlad6422 <vladok.malashuk@gmail.com>
2025-03-30 16:40:02 +0200

    feat : Old Transformer Model - dont use

--- MyTransformer/chatBot.py
+++
@@ -1,9 +0,0 @@
-# chatBot.py
-import processMovieCorpusDataset  # Ensure this is the correct module name
-# runDatasetDownload.py
-import datasetDownloader  # Ensure this is the correct module name
-if __name__ == "__main__":
-    config_file = 'config.json'  # Path to your config file
-    datasetDownloader.process_dataset(config_file)
-    processMovieCorpusDataset.process_dataset(config_file)
-    
